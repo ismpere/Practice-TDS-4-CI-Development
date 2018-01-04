000c7a670b0770f0f861a29e94a001d372ce978f
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-04 11:48:32 +0100

    Corrijo los argumentos del constructor y documento los metodos

--- src/main/java/uva/tds/pr4/ismpere/Persona.java
+++ src/main/java/uva/tds/pr4/ismpere/Persona.java
@@ -6,15 +6,26 @@ package uva.tds.pr4.ismpere;
  */
 public class Persona{
 
-	public Persona(String string, String string2) {
+	/**
+	 * Constructor por defecto de la clase Persona
+	 * @param id
+	 * @param nombre
+	 */
+	public Persona(String id, String nombre) {
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Devuelve el id de la Persona
+	 * @return id
+	 */
 	public String getId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	/**
+	 * Devuelve el nombre de la Persona
+	 * @return nombre
+	 */
 	public String getNombre() {
 		// TODO Auto-generated method stub
 		return null;
