1242f5ba78d0f5c4a8df1d4321ff600e882b8aa5
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-05 00:36:50 +0100

    Añado las suites de test para Persona y para todo el programa

---
+++ src/test/java/uva/tds/pr4/ismpere/AllTest.java
@@ -0,0 +1,17 @@
+package uva.tds.pr4.ismpere;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+    PersonaAllTest.class})
+
+/**
+ * Suite de test de todas las clases
+ * @author ismpere
+ */
+public class AllTest {
+
+}
