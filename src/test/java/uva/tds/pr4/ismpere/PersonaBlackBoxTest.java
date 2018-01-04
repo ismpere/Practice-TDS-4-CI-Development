396920dcd7fcc53539c15c18700efdc51c4be703
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-04 18:13:23 +0100

    Añado test de caja negra para los setId y setNombre

--- src/test/java/uva/tds/pr4/ismpere/PersonaBlackBoxTest.java
+++ src/test/java/uva/tds/pr4/ismpere/PersonaBlackBoxTest.java
@@ -23,6 +23,28 @@ public class PersonaBlackBoxTest {
 		assertEquals("1", a.getId());
 		assertEquals("CincoCincoCincoCincoCincoCincoCincoCincoCincoCinco", a.getNombre());
 	}
+	
+	@Test
+	public void testSetIdPersonaIdGrandeValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setId("123456789");
+		
+		assertNotNull(a);
+		assertEquals("123456789", a.getId());
+		assertEquals("a", a.getNombre());
+	}
+	
+	@Test
+	public void testSetNombrePersonaIdGrandeValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setNombre("CincoCincoCincoCincoCincoCincoCincoCincoCincoCinco");
+		
+		assertNotNull(a);
+		assertEquals("1", a.getId());
+		assertEquals("CincoCincoCincoCincoCincoCincoCincoCincoCincoCinco", a.getNombre());
+	}
 
 	@Test(expected = AssertionError.class)
 	public void testInicializaPersonaIdNuloNoValido() {
@@ -52,5 +74,47 @@ public class PersonaBlackBoxTest {
 	@Test(expected = AssertionError.class)
 	public void testInicializaPersonaNombreGrandeNoValido() {
 		Persona a = new Persona("1", "CincoCincoCincoCincoCincoCincoCincoCincoCincoCinco+");
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetIdPersonaIdNuloNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setId(null);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetIdPersonaIdGrandeNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setId("1234567890");
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetIdPersonaIdPequenioNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setId("");
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetNombrePersonaNombreNuloNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setNombre(null);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetNombrePersonaNombreGrandeNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setNombre("CincoCincoCincoCincoCincoCincoCincoCincoCincoCinco+");
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testSetNombrePersonaNombrePequenioNoValido() {
+		Persona a = new Persona("1", "a");
+		
+		a.setNombre("");
 	}
 }
