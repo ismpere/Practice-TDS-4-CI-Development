3b17c0494a3f2484d41d04b13ade6f1c98f3fe97
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-05 01:13:15 +0100

    Añado un test para comprobar si una Persona esta en la ColaDeAmigos

--- src/test/java/uva/tds/pr4/ismpere/ColaDeAmigosTDDTest.java
+++ src/test/java/uva/tds/pr4/ismpere/ColaDeAmigosTDDTest.java
@@ -23,6 +23,7 @@ public class ColaDeAmigosTDDTest {
 		ColaDeAmigos cola = new ColaDeAmigos(p);
 		
 		assertNotNull(cola);
+		assertArrayEquals(p, cola.getPersonas());
 	}
 	
 	@Test
@@ -39,5 +40,20 @@ public class ColaDeAmigosTDDTest {
 		assertNotNull(cola);
 		assertNotNull(p2);
 		assertArrayEquals(p2, p);
+	}
+	
+	@Test
+	public void testIsInColaDeAmigosTrueValido() {
+		Persona a = new Persona("1", "a");
+		Persona b = new Persona("1", "b");
+		Persona c = new Persona("3", "c");
+		Persona[] p = {a,b,c};
+		
+		ColaDeAmigos cola = new ColaDeAmigos(p);
+		
+		boolean is = a.isInColaDeAmigos(a);
+		
+		assertNotNull(cola);
+		assertTrue(is);
 	}
 }
