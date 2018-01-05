b58cc22fb1e87cd51999e54d02d41ba6d951352b
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-01-05 01:07:47 +0100

    Documento los metodos generados

--- src/main/java/uva/tds/pr4/ismpere/ColaDeAmigos.java
+++ src/main/java/uva/tds/pr4/ismpere/ColaDeAmigos.java
@@ -5,15 +5,24 @@ package uva.tds.pr4.ismpere;
  * @author ismpere
  */
 public class ColaDeAmigos{
-	
+	/**
+	 * Constructor por defecto sin argumentos de la clase ColaDeAmigos
+	 */
 	public ColaDeAmigos() {
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Cosntructor con argumentos de la clase ColaDeAmigos
+	 * @param p lista de Persona
+	 */
 	public ColaDeAmigos(Persona[] p) {
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Devuelve una lista de Persona de la ColaDeAmigos
+	 * Esta lista estará vacia si la ColaDeAmigos no tiene ninguna Persona
+	 * @return personasDeLaCola
+	 */
 	public Persona[] getPersonas() {
 		// TODO Auto-generated method stub
 		return null;
